e457511056a0aac313690604842b9017a30ce34e
Dr. Alto Clef <194142532+Gargleblaster-RBMK@users.noreply.github.com>
2026-02-19 17:51:26 -0600

    Add script to download and install debloater tools

--- clickme.ps1
+++ clickme.ps1
@@ -1,1 +1,8 @@
-
+#Requires -RunAsAdministrator
+#Requires -Version 5.1
+New-Item -Path "~/Downloads/" -Name "Debloater" -ItemType "Directory"
+cd ~/Downloads/Debloater/
+Invoke-WebRequest -HttpVersion 2.0 -uri "https://github.com/farag2/Sophia-Script-for-Windows/releases/download/7.1.2/Sophia.Script.for.Windows.10.LTSC.2019.v6.1.2.zip"
+Expand-Archive -Path "Sophia.Script.for.Windows.10.LTSC.2019.v6.1.2.zip" -DestinationPath ~/Downloads/Debloater/
+./Sophia.ps1
+winget install Librewolf Krita Inkscape 
